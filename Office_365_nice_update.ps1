913a859a41d559e5dcc64c5728a45774c3cc58cb
Daniel Keer <4249262+thedxt@users.noreply.github.com>
2021-04-14 16:46:56 -0600

    Update Office_365_nice_update.ps1
    fixed typo

--- Office_365_nice_update.ps1
+++ Office_365_nice_update.ps1
@@ -1,1 +1,1 @@
-cmd /c "C:\Program Files\Common Files\microsoft shared\ClickToRun\OfficeC2RClient.exe" /update use
+cmd /c "C:\Program Files\Common Files\microsoft shared\ClickToRun\OfficeC2RClient.exe" /update user
