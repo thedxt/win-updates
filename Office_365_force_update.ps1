d20ff9bd2e74e42770cf8b671786e71f1c127171
Daniel Keer <4249262+thedxt@users.noreply.github.com>
2021-04-15 07:33:03 -0600

    added detection and info messages
    added detection for if office is install and added info on what the script is doing to the output

--- Office_365_force_update.ps1
+++ Office_365_force_update.ps1
@@ -1,1 +1,44 @@
+# Check is Office Click to Run exists
+if (Test-Path "C:\Program Files\Common Files\microsoft shared\ClickToRun\OfficeC2RClient.exe")
+
+# run Force updates if found
+{
+Write-Host "Office Click To Run is found"
+Write-host "Forcing Office to be closed"
+
+# Ignore errors if office apps aren't open
+$ErrorActionPreference = 'SilentlyContinue'
+
+# kill word
+#Get-Process -Name winword | Stop-Process -Force
+
+# kill excel
+#Get-Process -Name excel | Stop-Process -Force
+
+# Kill outlook
+#Get-Process -Name outlook | Stop-Process -Force
+
+# kill powerpoint
+#Get-Process -Name POWERPNT | Stop-Process -Force
+
+# kill onenote
+#Get-Process -Name "*onenote*" | Stop-Process -Force
+
+# kill publisher
+#Get-Process -Name MSPUB | Stop-Process -Force
+
+# kill MS Access
+#Get-Process -Name MSACCESS | Stop-Process -Force
+
+# kill OfficeClickToRun (might be the only one needed)
+Get-Process -Name OfficeClickToRun | Stop-Process -Force
+
+Write-Host "Running Forced Updates"
+
 cmd /c “C:\Program Files\Common Files\Microsoft Shared\ClickToRun\OfficeC2RClient.exe” /update user updatepromptuser=false forceappshutdown=true displaylevel=false
+}
+else
+# don't do anything if not found
+{
+Write-Host "Office Click To Run is not found"
+}
