3937c8bd8723f4d5a4d0c184f5e68e2c7eaa3cdd
Daniel Keer <4249262+thedxt@users.noreply.github.com>
2021-04-15 07:41:16 -0600

    Update and rename Office_365_nice_update.ps1 to DK_Office_365_nice_update.ps1
    fixed file name and messaging

--- DK_Office_365_nice_update.ps1
+++ DK_Office_365_nice_update.ps1
@@ -1,10 +1,10 @@
 # Check is Office Click to Run exists
 if (Test-Path "C:\Program Files\Common Files\microsoft shared\ClickToRun\OfficeC2RClient.exe")
 
-# run nice updates if found
+# run nice Office updates if found
 {
 Write-Host "Office Click To Run is found"
-Write-Host "Running Nice Updates"
+Write-Host "Running Nice Office Updates"
 cmd /c "C:\Program Files\Common Files\microsoft shared\ClickToRun\OfficeC2RClient.exe" /update user
 }
 else
