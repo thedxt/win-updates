7cc040e3432d9143235bfa5d71e4e69b1d614bd2
Daniel Keer <4249262+thedxt@users.noreply.github.com>
2021-04-15 06:51:01 -0600

    install detection
    added Office Install detection

--- Office_365_nice_update.ps1
+++ Office_365_nice_update.ps1
@@ -1,1 +1,14 @@
+# Check is Office Click to Run exists
+if (Test-Path "C:\Program Files\Common Files\microsoft shared\ClickToRun\OfficeC2RClient.exe")
+
+# run nice updates if found
+{
+Write-Host "Office Click To Run is found"
+Write-Host "Running Nice Updates"
 cmd /c "C:\Program Files\Common Files\microsoft shared\ClickToRun\OfficeC2RClient.exe" /update user
+}
+else
+# don't do anything if not found
+{
+Write-Host "Office Click To Run is not found"
+}
