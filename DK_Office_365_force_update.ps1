df6d3c6fd4c840b28777b62f1f9d3697a6e5e2ba
Daniel Keer <4249262+thedxt@users.noreply.github.com>
2021-04-15 07:44:46 -0600

    Update and rename Office_365_force_update.ps1 to DK_Office_365_force_update.ps1
    fixed the naming and cleaned up the coding

--- DK_Office_365_force_update.ps1
+++ DK_Office_365_force_update.ps1
@@ -9,36 +9,15 @@ Write-host "Forcing Office to be closed"
 # Ignore errors if office apps aren't open
 $ErrorActionPreference = 'SilentlyContinue'
 
-# kill word
-#Get-Process -Name winword | Stop-Process -Force
-
-# kill excel
-#Get-Process -Name excel | Stop-Process -Force
-
-# Kill outlook
-#Get-Process -Name outlook | Stop-Process -Force
-
-# kill powerpoint
-#Get-Process -Name POWERPNT | Stop-Process -Force
-
-# kill onenote
-#Get-Process -Name "*onenote*" | Stop-Process -Force
-
-# kill publisher
-#Get-Process -Name MSPUB | Stop-Process -Force
-
-# kill MS Access
-#Get-Process -Name MSACCESS | Stop-Process -Force
-
-# kill OfficeClickToRun (might be the only one needed)
+# kill OfficeClickToRun to help office force the updates
 Get-Process -Name OfficeClickToRun | Stop-Process -Force
 
-Write-Host "Running Forced Updates"
+Write-Host "Running Forced Office Updates"
 
 cmd /c “C:\Program Files\Common Files\Microsoft Shared\ClickToRun\OfficeC2RClient.exe” /update user updatepromptuser=false forceappshutdown=true displaylevel=false
 }
 else
-# don't do anything if not found
+# don't do anything if Click to Run is not found
 {
 Write-Host "Office Click To Run is not found"
 }
